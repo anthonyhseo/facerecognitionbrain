c9bfed015d688bcd9bbea36fd992509a8789d2db
unknown <anthonyhseo@gmail.com>
2018-02-12 13:54:02 -0800

    updating PORT variable

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/imageurl', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is up and running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is up and running on port ${process.env.PORT}`);
 });
 
